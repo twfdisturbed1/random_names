7048f099b8d58c62aa892b8f06330bba4e17bb64
William Mullins <twfdisturbed@hotmail.com>
2018-07-09 15:40:27 -0400

    Testing things

---
+++ random_names.rb
@@ -0,0 +1,14 @@
+def people()
+  peeps = ["bill", "sally", "Jim", "Mary", "Mike"]
+
+end
+
+# def people ()
+# group_1 = ["bill", "sally", "Jim", "Mary", "Mike"]
+# group_2 = []
+# num = arr1.lenght%2
+# arr1.each do |name|
+#   if new_arr1.lenght! = 2
+#     new_arr1 << name
+#   end
+# end
--- tdd_test.rb
+++ tdd_test.rb
@@ -6,4 +6,11 @@ class TestCodes < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
+    def test_if_array_is_array
+        assert_equal(Array, people().class)
+    end
+    def test_if_array_is_has_elements
+    assert_equal(["bill", "sally", "Jim", "Mary", "Mike"], people())
+    end
 end
+
